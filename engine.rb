37d52c942c856094183cac41260d98f3573dbcd5
bomatson <bobby@carbonfive.com>
2014-02-19 15:35:46 -0800

    give the drums dynamics

--- engine.rb
+++ engine.rb
@@ -14,9 +14,10 @@ class DrumSet < Artoo::MainRobot
   end
 
   def initialize
-    @low_tom = Surface.new(0, 100, 36)
-    @high_tom = Surface.new(-100, 0, 38)
-    @drums = [@low_tom, @high_tom]
+    @snare = Surface.new(0, 100, 36)
+    @bass_drum = Surface.new(-51, 0, 38)
+    @hit_hat = Surface.new(-100, -50, 57)
+    @drums = [@snare, @bass_drum, @hit_hat]
     @@finger = nil
     @previous = {}
     super
@@ -29,26 +30,50 @@ class DrumSet < Artoo::MainRobot
     frame = args[1]
     return if frame.nil?
 
+    #@@finger = frame.pointables.reduce(nil) { |old, point|
+    #  if old == nil || (point.tipPosition[1] < old.tipPosition[1]) && point.length > 40
+    #    p 'new'
+    #    point
+    #  else
+    #    p 'OLD'
+    #    old
+    #  end
+    #}
+
     @@finger = frame.pointables.detect do |point|
-      point.timeVisible > 1.5
+      p 'point'
+      point.length > 40
     end
 
     if @@finger
       x_position = @@finger.tipPosition[0]
       y_position = @@finger.tipPosition[1]
       y_velocity = @@finger.tipVelocity[1]
-      p y_velocity
     end
+
+    #frame.pointables.delete(@@finger)
+
+    #@@second_finger = frame.pointables.detect do |point|
+    #  point.timeVisible > 0.5
+    #end
+
+    #if @@second_finger
+     # p 'have a second'
+     # byebug
+    #end
 
     return unless y_velocity
-    return unless y_velocity.abs > 1000
+    #return unless y_velocity.abs > 1000
 
-    if is_a_hit?(frame.timestamp, y_velocity)
-      drum_for(@drums, x_position)
+    puts "*" * (y_position/10).to_i
+
+    if is_a_hit?(frame.timestamp, y_position)
+      drum_for(@drums, x_position, y_velocity)
     end
+    @previous[:y_position] = y_position
 
-    @previous[:timestamp] = frame.timestamp
-    @previous[:y_velocity] = y_velocity
+    #@previous[:timestamp] = frame.timestamp
+    #@previous[:y_velocity] = y_velocity
   end
 
   def on_close(*args)
@@ -57,21 +82,31 @@ class DrumSet < Artoo::MainRobot
 
   private
 
-  def is_a_hit?(timestamp, y_velocity)
-    if @previous[:y_velocity] && y_velocity && @previous[:y_velocity] > 0 && y_velocity < 0 && (timestamp - @previous[:timestamp] > 500)
-      puts "#{timestamp - @previous[:timestamp]} --- #{y_velocity}"
-      #puts "#{timestamp}: #{@previous[:y_velocity]} --- #{y_velocity}"
+  #def is_a_hit?(timestamp, y_velocity)
+  # if @previous[:y_velocity] && y_velocity && @previous[:y_velocity] > 0 && y_velocity < 0 && (timestamp - @previous[:timestamp] > 500)
+    # puts "#{timestamp - @previous[:timestamp]} --- #{y_velocity}"
+  #    puts "#{timestamp}: #{@previous[:y_velocity]} --- #{y_velocity}"
+  #    return true
+  #  end
+  #  false
+  #end
+
+  def is_a_hit?(timestamp, y_position)
+    if @previous[:y_position] && y_position && @previous[:y_position] > 150 && y_position < 150
+      puts "#{y_position} -- #{@previous[:y_position]}"
+      puts 'passed 200'
       return true
+    else
+      false
     end
-    false
   end
 
-  def drum_for(drums, x_position)
-    drum_hit = drums.select do |drum|
+  def drum_for(drums, x_position, y_velocity)
+    @volume = [[ 0, (y_velocity.abs/30).to_i ].max, 100].min
+    drum_hit = drums.detect do |drum|
       x_position > drum.left_boundary && x_position < drum.right_boundary
-    end.first
-
-    drum_hit.play if drum_hit
+    end
+    drum_hit.play(@volume) if drum_hit
   end
 end
 
--- lib/surface.rb
+++ lib/surface.rb
@@ -1,11 +1,11 @@
 class Surface < Struct.new(:left_boundary, :right_boundary, :drum_note)
-  def play
+  def play(volume)
     output = UniMIDI::Output.open(:first)
 
     output.open do |node|
-       node.puts(0x90, drum_note, 100)
+       node.puts(0x90, drum_note, volume)
        sleep(0.1)
-       node.puts(0x80, drum_note, 100)
+       node.puts(0x80, drum_note, volume)
     end
   end
 end
