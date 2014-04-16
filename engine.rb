c9c3b9873aaf0a7fc57848ac3a76ba97ce3f8c06
bomatson <bobby@carbonfive.com>
2014-04-16 15:42:41 -0700

    wip

--- engine.rb
+++ engine.rb
@@ -15,79 +15,65 @@ class DrumSet < Artoo::MainRobot
 
   def initialize
     @snare = Surface.new(0, 100, 36)
-    @bass_drum = Surface.new(-51, 0, 38)
+    @bass_drum = Surface.new(-49, -1, 38)
     @hit_hat = Surface.new(-100, -50, 57)
     @drums = [@snare, @bass_drum, @hit_hat]
     @@finger = nil
-    @previous = {}
+    @previous = {y_position: [] }
     super
   end
 
   def on_open(*args)
   end
 
+  def cleanup
+    @previous[:y_position] = []
+  end
+
   def on_frame(*args)
     frame = args[1]
 
-    return if frame.hands.empty?
+    cleanup if frame.hands.empty?
+    #puts "hands: #{frame.hands.count}"
 
-    if frame.hands.length == 1
-      @hand = frame.hands.first
-      #@hand = frame.hands.detect{ |hand| hand.timeVisible > 0.5 }
-
-      x_position = @hand.palmPosition[0]
-      y_position = @hand.palmPosition[1]
-      y_velocity = @hand.palmVelocity[1]
-      print "\r"
-      (y_position/10).to_i.times do
-        print "*"
+    #if frame.hands.length == 1
+    frame.hands.each_with_index do |hand, index| 
+      if is_a_hit?(hand, index)
+        drum_for(@drums, hand.palmPosition[0], hand.palmVelocity[1])
       end
-      print "                                    "
-
-      if is_a_hit?(y_position)
-        drum_for(@drums, x_position, y_velocity)
-      end
-
-      @previous[:y_position] = y_position
-    elsif frame.hands.length == 2
-      @first_hand = frame.hands[0]
-      @second_hand = frame.hands[1]
-
-      @active = [@first_hand, @second_hand].detect{ | hand| hand.palmPosition[1] < 150 }
-      return if @active.nil?
-      p @active.id
-
-      active_y_position = @active.palmPosition[1]
-
-      if is_a_hit?(active_y_position)
-        drum_for(@drums, @active.palmPosition[0], @active.palmVelocity[1])
-      end
-
-      @previous[:y_position] = active_y_position
+      @previous[:y_position][index] = hand.palmPosition[1] 
     end
-    #
-    #if @hands.count == 1
-    #  x_position[0] = @hands.first.palmPosition[0]
-    #  y_position[0] = @hands.first.palmPosition[1]
-    #  y_velocity[0] = @hands.first.palmVelocity[1]
-    #  puts "*" * (y_position[0]/10).to_i
-    #elsif @hands.count > 1
-    #  @hands.each_with_index do |hand, index|
-    #    x_position[index] = hand.palmPosition[0]
-    #    y_position[index] = hand.palmPosition[1]
-    #    y_velocity[index] = hand.palmPosition[1]
+    #@hand = frame.hands.first
+
+    #x_position = @hand.palmPosition[0]
+    #y_position = @hand.palmPosition[1]
+    #y_velocity = @hand.palmVelocity[1]
+
+    #if is_a_hit?(@hand, )
+    #end
+
+    #@previous[:y_position][:first] =  y_position
+    #elsif frame.hands.length == 2
+    #  @first_hand = frame.hands[0]
+    #  @second_hand = frame.hands[1]
+
+
+    #  #@active = [@first_hand, @second_hand].detect{ | hand| hand.palmPosition[1] < 150 }
+    #  @active = nil
+
+    #  [@first_hand, @second_hand].each_with_index do |hand, index|
+    #    puts index
+    #    if is_a_hit?(hand, index)
+    #      @active = hand
+    #    end
     #  end
-    #  puts "X" * (y_position[1]/10).to_i
+
+    #  return if @active.nil?
+    #  drum_for(@drums, @active.palmPosition[0], @active.palmVelocity[1])
+
+    #  @previous[:y_position][:first] =  @first_hand.palmPosition[1]
+    #  @previous[:y_position][:second] = @second_hand.palmPosition[1]
     #end
-
-
-    #if @@hands.length == 1
-    #elsif @@hands.length > 1
-    #end
-
-
-    #@previous[:timestamp] = frame.timestamp
-    #@previous[:y_velocity] = y_velocity
   end
 
   def on_close(*args)
@@ -96,21 +82,19 @@ class DrumSet < Artoo::MainRobot
 
   private
 
-  #def is_a_hit?(timestamp, y_velocity)
-  # if @previous[:y_velocity] && y_velocity && @previous[:y_velocity] > 0 && y_velocity < 0 && (timestamp - @previous[:timestamp] > 500)
-    # puts "#{timestamp - @previous[:timestamp]} --- #{y_velocity}"
-  #    puts "#{timestamp}: #{@previous[:y_velocity]} --- #{y_velocity}"
-  #    return true
-  #  end
-  #  false
-  #end
+  def is_a_hit?(hand, index)
+    #if index == 0
+    #  key = :first
+    #elsif index == 1
+    #  key = :second
+    #else
+    #  return
+    #end
 
-  def is_a_hit?(y_position)
-
-    if @previous[:y_position] && y_position && @previous[:y_position] > 150 && y_position < 150
-      puts "#{y_position} -- #{@previous[:y_position]}"
-      puts 'passed 200'
-      return true
+    #puts key
+    if @previous[:y_position][index] && hand.palmPosition[1] && @previous[:y_position][index] > 150 && hand.palmPosition[1] < 150
+      puts "#{hand.palmPosition[1]} -- #{@previous[:y_position][index]}"
+      true
     else
       false
     end
--- lib/surface.rb
+++ lib/surface.rb
@@ -4,8 +4,8 @@ class Surface < Struct.new(:left_boundary, :right_boundary, :drum_note)
 
     output.open do |node|
        node.puts(0x90, drum_note, volume)
-       sleep(0.1)
-       node.puts(0x80, drum_note, volume)
+       #sleep(0.1)
+       #node.puts(0x80, drum_note, volume)
     end
   end
 end
